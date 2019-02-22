1714d67d8135b9e30c7eb1169637ada72e005ae6
leadinmyhead <cdavis5@uccs.edu>
2019-02-22 02:37:07 +0000

    test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,12 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    if params.key?(:sort_by)
-		session[:sort_by] = params[:sort_by]
-	elsif session.key?(:sort_by)
-		params[:sort_by] = session[:sort_by]
-		redirect_to movies_path(params) and return
-	end
+    @movies = Movie.find(:all, :order=>(params[:sort_by]))
     @sort_columns = params[:sort_by]
   end
 
